5d51e49f259a833eaabd06324262d4b6187b42ef
Martyn <martyn.barac@gmail.com>
2020-06-25 15:40:09 +1000

    Added handshake functionality

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 
 class clientSock:
@@ -6,25 +7,71 @@ class clientSock:
         self.IP = ip
         self.PORT = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.CONNECTION_TIMEOUT = 5
+        self.sock.settimeout(1/60)
 
     def __del__(self):
         self.close()
 
-    def send_msg(self, msg, address):
-        self.sock.sendto(msg, address)
+    def send_msg(self, msg):
+        self.sock.sendto(msg, (self.IP,self.PORT))
         return None
 
-    def recieve_from_server(self):
-        message = self.sock.recvfrom(1024)
-        return message[0]
+    def send_msg_handshake(self, msg):
+        """
+        Repeatedly sends the server a message until a handshake is received.
+        Use this if a message needs to get across to the server reliably
+
+        :param msg: The msg to send
+        :return: Returns True if handshake successful
+        """
+        print("handshaking...")
+        time_start = time.perf_counter()
+        while True:
+            self.sock.sendto(b'h,'+msg, (self.IP, self.PORT))
+            try:
+                received_message = self.receive_from_server()
+                if received_message == b'h2,'+msg:
+                    print("handshake successful")
+                    return True
+            except TimeoutError:
+                pass
+            time_end = time.perf_counter()
+            duration = time_end-time_start
+            if duration > self.CONNECTION_TIMEOUT:
+                print("handshake error: timed out")
+                return False
+
+            time.sleep(1/60)
+
+    def receive_from_server(self):
+        """
+        Will return a message if received
+        Else will return empty string
+        """
+        try:
+            msg, addr = self.sock.recvfrom(1024)
+        except TimeoutError:
+            msg = b''
+        return msg
 
     def close(self):
         self.sock.close()
 
-my_client = clientSock("127.0.0.1", 27014)
+
+addr = input("Enter server address: ")
+addr = addr.split(":")
+if len(addr) > 1:
+    PORT = int(addr[1])
+
+else:
+    PORT = 27014
+
+IP = addr[0]
+
+my_client = clientSock(IP, PORT)
 
 message_to_send = input("Send a message: ")
 while message_to_send:
-    my_client.send_msg(message_to_send.encode(), ("127.0.0.1", 27014))
-    print(my_client.recieve_from_server())
+    my_client.send_msg_handshake(message_to_send.encode())
     message_to_send = input("Send a message: ")
--- server.py
+++ server.py
@@ -5,44 +5,45 @@ class serverSock:
     def __init__(self, ip, port):
         self.IP = ip
         self.PORT = port
-        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.IP, self.PORT))
 
     def __del__(self):
         self.close()
 
-    def send_msg(self, msg, address):
-        self.sock.sendto(msg, address)
+    def send_msg(self, msg, addr):
+        self.sock.sendto(msg, addr)
         return None
 
     def recv_all(self):
-        data, addr = self.sock.recvfrom(1024)
-        print(data)
-        return data, addr
+        msg, addr = self.sock.recvfrom(1024)
+        return msg, addr
 
-    def recv_and_echo(self):
-        received = self.recv_all()
-        self.send_msg(received[0], received[1])
-        return data, addr
+
+    def recv_and_handshake(self):
+        """
+        Recieves and returns message, will handshake if asked.
+
+        :return: returns messgae and adress from the client that sent a message
+        """
+        msg, addr = self.recv_all()
+        msg = msg.split(b',')
+
+        # If the message begins with h then the client wants a handshake
+        # echo it back for a handshake
+        if msg[0] == b'h':
+            print("Handshaking..")
+            new_msg = b'h2,'+b','.join(msg[1:])
+            self.send_msg(new_msg, addr)
+
+        return msg, addr
 
     def close(self):
         self.sock.close()
 
-user_choice = input("Send message Y/N? ")
-
-if user_choice == "N":
-    print("Initialising")
-    my_server = serverSock("127.0.0.1", 27014)
-    while True:
-        print("working")
-        print(my_server.recv_and_echo())
-
-else:
-    my_server = serverSock("127.0.0.1", 27014)
-    message_to_send = input("Enter message: ")
-    while message_to_send:
-        my_server.send_msg(message_to_send.encode(), "127.0.0.1", 27014)
-        message_to_send = input("Enter message: ")
 
 
-
+print("Initialising")
+my_server = serverSock("127.0.0.1", 27014)
+while True:
+    print(my_server.recv_and_handshake()[0])
