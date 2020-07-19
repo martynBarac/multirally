151f2003673efdffb401f4796591982e947bf72a
Ev01 <evpevdev@optusnet.com.au>
2020-07-19 16:15:32 +1000

    added draw function

--- powerup.py
+++ powerup.py
@@ -1,9 +1,11 @@
+import pygame as pg
 
 class Powerup:
 	def __init__(self, x, y, typ):
 		self.xpos = x
 		self.ypos = y
 		self.type = typ
+		self.rect = pg.Rect(x, y, 8, 8)
 		
 	def draw(self):
-		pass
+		return self.rect
