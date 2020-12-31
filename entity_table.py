6f006c5c8d22ea90ae63b6a22ecc1be6cf9a451e
Martyn <martyn.barac@gmail.com>
2021-01-01 02:26:06 +1100

    ev u forgot a comma

--- entity_table.py
+++ entity_table.py
@@ -3,6 +3,6 @@ import powerup
 
 entity_table = {
 #  id: ( Serverclass , Clientclass   )
-    1: (player.Player, player.CPlayer)
+    1: (player.Player, player.CPlayer),
     2: (powerup.Powerup, powerup.CPowerup)
 }
