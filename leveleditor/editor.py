ed8b1c51710548a464d61bbaf256d625e09acc3f
Ev01 <evpevdev@optusnet.com.au>
2021-01-03 18:54:10 +1100

    simple box creation

--- leveleditor/editor.py
+++ leveleditor/editor.py
@@ -2,7 +2,11 @@ import pygame as pg
 
 class Editor:
     def __init__(self):
-        pass
+        pg.init()
+        self.level = []
+        self.box_start = (0, 0)
+        self.mouse_down = False
+
 
     def load_level(self):
         pass
@@ -18,9 +22,41 @@ class Editor:
     def update(self):
         pass
 
-    def event_handle(self):
-        pass
+    def add_box(self, x1, y1, x2, y2):
+        w = h = x = y = 0
+        if x1 < x2:
+            w = x2 - x1
+            x = x1
+        else:
+            w = x1 - x2
+            x = x2
 
-    def draw(self):
-        pass
+        if y1 < y2:
+            h = y2 - y1
+            y = y1
+        else:
+            h = y1 - y2
+            y = y2
+
+        self.level.append([x, y, w, h])
+
+    def event_handle(self):
+        for event in  pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.box_start = pg.mouse.get_pos()
+                    self.mouse_down = True
+            elif event.type == pg.MOUSEBUTTONUP:
+                if event.button == 1:
+                    self.mouse_down = False
+                    box_end = pg.mouse.get_pos()
+                    self.add_box(self.box_start[0], self.box_start[1], box_end[0], box_end[1])
+
+    def draw(self, screen):
+        pg.draw.rect(screen, (255, 255, 255), [0, 0, 10, 10])
+        for box in self.level:
+            pg.draw.rect(screen, (255, 255, 255), box)
+        pg.display.update()
 
--- leveleditor/main.py
+++ leveleditor/main.py
@@ -1,4 +1,14 @@
+import pygame as pg
 import editor
+
+
+SCREEN_SIZE = (640, 480)
+
+pg.init()
+screen = pg.display.set_mode(SCREEN_SIZE)
+
 e = editor.Editor()
 while True:
-    e.run()
+    e.event_handle()
+    e.update()
+    e.draw(screen)
