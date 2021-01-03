0e57e41e0f68e99079588c5bce380cabc28ab104
Ev01 <evpevdev@optusnet.com.au>
2021-01-03 18:59:47 +1100

    outline of box while drawing

--- leveleditor/editor.py
+++ leveleditor/editor.py
@@ -5,7 +5,7 @@ class Editor:
         pg.init()
         self.level = []
         self.box_start = (0, 0)
-        self.mouse_down = False
+        self.drawing = False
 
 
     def load_level(self):
@@ -23,6 +23,11 @@ class Editor:
         pass
 
     def add_box(self, x1, y1, x2, y2):
+
+        box = self.get_box(x1, y1, x2, y2)
+        self.level.append(box)
+
+    def get_box(self, x1, y1, x2, y2):
         w = h = x = y = 0
         if x1 < x2:
             w = x2 - x1
@@ -38,7 +43,7 @@ class Editor:
             h = y1 - y2
             y = y2
 
-        self.level.append([x, y, w, h])
+        return [x, y, w, h]
 
     def event_handle(self):
         for event in  pg.event.get():
@@ -47,15 +52,20 @@ class Editor:
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     self.box_start = pg.mouse.get_pos()
-                    self.mouse_down = True
+                    self.drawing = True
             elif event.type == pg.MOUSEBUTTONUP:
                 if event.button == 1:
-                    self.mouse_down = False
+                    self.drawing = False
                     box_end = pg.mouse.get_pos()
                     self.add_box(self.box_start[0], self.box_start[1], box_end[0], box_end[1])
 
     def draw(self, screen):
-        pg.draw.rect(screen, (255, 255, 255), [0, 0, 10, 10])
+        screen.fill((0, 0, 0))
+
+        if self.drawing:
+            mouse = pg.mouse.get_pos()
+            pg.draw.rect(screen, (128, 128, 128), self.get_box(self.box_start[0], self.box_start[1], mouse[0], mouse[1]), 2)
+
         for box in self.level:
             pg.draw.rect(screen, (255, 255, 255), box)
         pg.display.update()
